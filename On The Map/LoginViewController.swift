6a5defe4f2973c9ac35ccbec6f4e92bace572cc3
Christopher Luc <christopher.e.luc@gmail.com>
2016-02-16 22:26:11 -0800

    Created initial login screen

--- On The Map/LoginViewController.swift
+++ On The Map/LoginViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupText: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
